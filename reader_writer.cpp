86495ab63136c44b0c433d7bc3ce13077c852079
胡其成 <huqicheng@huqicheng.local>
2017-11-25 18:19:45 -0500

    first commit

--- reader_writer.cpp
+++ reader_writer.cpp
@@ -1,3 +1,6 @@
+#include "minisat/core/SolverTypes.h"
+#include "minisat/core/Solver.h"
+#include "minisat/mtl/Vec.h"
 #include "helper.h"
 #include <cstdio>
 #include <iostream>
@@ -10,6 +13,86 @@
 
 using namespace std;
 using namespace stringutils;
+
+// encapsulate Minisat::Solver in SatSolver to adapt the graph structure
+class SatSolver{
+    private:
+    Minisat::Solver* solver;
+    vector<vector<Minisat::Lit> > vectLits;
+    int row,col;
+    public:
+    SatSolver(){
+        solver = new Minisat::Solver();
+    }
+    void setRow(int r){row = r;}
+    void setCol(int c){col = c;}
+    int getRow(){return row;}
+    int getCol(){return col;}
+    void addClause(vector<vector<int> > idx, vector<int> sign);
+    void addLiteral();
+    Minisat::lbool solve();
+    void resetSolver();
+    void getModel(vector<vector<int> >& res);
+    ~SatSolver(){
+        delete solver;
+    }
+};
+
+void SatSolver::addClause(vector<vector<int> > idx, vector<int> sign){
+    Minisat::vec<Minisat::Lit> vect;
+    for(unsigned int i=0; i<idx.size(); ++i){
+        
+        vector<int> index = idx[i];
+        int s = sign[i];
+        if(s == 0){
+            vect.push(~vectLits[index[0]][index[1]]);
+        }else if(s == 1){
+            vect.push(vectLits[index[0]][index[1]]);
+        }
+    }
+    
+    solver->addClause(vect);
+}
+
+void SatSolver::addLiteral(){
+    for(int i=0; i<row; ++i){
+        vector<Minisat::Lit> tmp;
+        for(int j=0;j<col;++j){
+            tmp.push_back(Minisat::mkLit(solver->newVar()));
+        }
+        vectLits.push_back(tmp);
+    }
+}
+
+Minisat::lbool SatSolver::solve(){
+    if(!solver->simplify()){
+        cout<<col<<" cannot be simplified" << endl;
+        return Minisat::l_False;
+    }
+    Minisat::vec<Minisat::Lit> dummy;
+    return solver->solveLimited(dummy);
+}
+
+void SatSolver::resetSolver(){
+    //reset solver
+    delete solver;
+    solver = new Minisat::Solver();
+    //reset lits
+    vectLits.clear();
+    
+}
+
+// get the assignment which satisfies CNF
+void SatSolver::getModel(vector<vector<int> >& res){
+    for(int i=0;i<row;++i){
+        vector<int> tmp;
+        for(int j=0;j<col;++j){
+            tmp.push_back(Minisat::toInt(solver->modelValue(vectLits[i][j])));
+        }
+        res.push_back(tmp);
+    }
+}
+
 
 pthread_mutex_t mut_output = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mut_input = PTHREAD_MUTEX_INITIALIZER;
@@ -38,18 +121,174 @@ void reset(){
 
 
 void printResult(string name,vector<int>& result){
-    cout << name << ":";
+    cout << name << ": ";
     for(unsigned int i=0;i<result.size();i++){
         if(i != result.size()-1)
             cout<<result[i]<<",";
         else
-            cout<<result[i]<<endl;
+            cout<<result[i];
     }
+    cout<<endl;
 }
+
+
+
+
+// to check if k is a correct size of vertex cover of the graph
+Minisat::lbool solve(Graph& g , int k, vector<vector<int> >& res){
+    
+    // initialize the solver
+    SatSolver solver;
+    solver.setRow(g.getVertexNum());
+    solver.setCol(k);
+    solver.addLiteral();
+    
+    //encoding 1
+    for(int i=0;i<k;++i){
+        vector<vector<int> > idx;
+        vector<int> sign;
+        for(int j=0;j<g.getVertexNum();++j){
+            vector<int> index;
+            index.push_back(j);
+            index.push_back(i);
+            idx.push_back(index);
+            sign.push_back(1);
+        }
+        solver.addClause(idx,sign);
+    }
+    
+    //encoding 2
+    for(int i=0;i<g.getVertexNum();++i){
+        for(int p=0;p<k;++p){
+            for(int q=p+1;q<k;++q){
+                vector<vector<int> > idx;
+                vector<int> sign;
+                
+                vector<int> index;
+                index.push_back(i);
+                index.push_back(p);
+                idx.push_back(index);
+                sign.push_back(0);
+                
+                vector<int> index2;
+                index2.push_back(i);
+                index2.push_back(q);
+                idx.push_back(index2);
+                sign.push_back(0);
+                
+                solver.addClause(idx,sign);
+            }
+        }
+    }
+    
+    //encoding 3
+    for(int i=0;i<k;++i){
+        for(int p=0;p<g.getVertexNum();++p){
+            for(int q=p+1;q<g.getVertexNum();++q){
+                vector<vector<int> > idx;
+                vector<int> sign;
+                
+                vector<int> index;
+                index.push_back(p);
+                index.push_back(i);
+                idx.push_back(index);
+                sign.push_back(0);
+                
+                vector<int> index2;
+                index2.push_back(q);
+                index2.push_back(i);
+                idx.push_back(index2);
+                sign.push_back(0);
+                
+                solver.addClause(idx,sign);
+            }
+        }
+    }
+    
+    //encoding 4
+    vector<vector<int> > edges;
+    g.getEdges(edges);
+    for(unsigned int a=0;a<edges.size();++a){
+        int i = edges[a][0];
+        int j = edges[a][1];
+        vector<vector<int> > idx;
+        vector<int> sign;
+        for(int s = 0;s<k;++s){
+            vector<int> index;
+            index.push_back(i);
+            index.push_back(s);
+            idx.push_back(index);
+            sign.push_back(1);
+            
+            vector<int> index2;
+            index2.push_back(j);
+            index2.push_back(s);
+            idx.push_back(index2);
+            sign.push_back(1);
+        }
+        solver.addClause(idx,sign);
+    }
+    //solve SAT
+    
+    Minisat::lbool tag = solver.solve();
+    if(tag == Minisat::l_True){
+        solver.getModel(res);
+    }
+    return tag;
+}
+
+
+
+
+void* solveBySat(Graph& g){
+    int left=1, right=g.getVertexNum()-1;
+    int resK = -1;
+    vector<int> resV;
+    while(left<=right){
+        int k = (right-left)/2+left;
+        
+        vector<vector<int> > res;
+        Minisat::lbool tag = solve(g,k,res);
+        
+        // if tag == true, then current k is guaranteed to be the minimum
+        if(tag == Minisat::l_True){
+            vector<int> vertices;
+            // vector vertices is guaranteed to be in ascending order
+            for(unsigned int ii = 0;ii<res.size();++ii){
+                for(unsigned int jj=0;jj<res[ii].size();++jj){
+                    if(res[ii][jj] == 0){
+                        vertices.push_back(ii);
+                    }
+                }
+            }
+            
+            resK = k;
+            resV = vertices;
+            
+            right = k-1;
+            
+        }else{
+            left = k+1;
+            
+        }
+        
+    }
+    for(unsigned int ii=0;ii<resV.size();++ii){
+        if(ii == resV.size()-1){
+            cout<<resV[ii]<<endl;
+        }else{
+            cout<<resV[ii]<<" ";
+        }
+        
+    }
+    
+}
+
+
 
 /* to simulate 3 algorithms :(*/
 
-//a1 is to mutiply initial by 1
+//approx 2
 void* a1(void* args){
     
     mulock(LOCK,&mut_input);
@@ -96,7 +335,7 @@ void* a1(void* args){
     mulock(UNLOCK,&mut_input);
 }
 
-//a2 is to mutiply initial by 2
+//approx 1
 void* a2(void* args){
     
     mulock(LOCK,&mut_input);
@@ -145,12 +384,25 @@ void* a2(void* args){
     mulock(UNLOCK,&mut_input);
 }
 
-//a3 is to mutiply initial by 3
+//sat
 void* a3(void* args){
     
     mulock(LOCK,&mut_input);
     
-    //algorithm
+    for(int k=1; k<g.getVertexNum()-1;k++){
+        vector<vector<int> > res;
+        Minisat::lbool tag = solve(g,k,res);
+        if(tag == Minisat::l_True){
+            for(unsigned int ii = 0;ii<res.size();++ii){
+                for(unsigned int jj=0;jj<res[ii].size();++jj){
+                    if(res[ii][jj] == 0){
+                        resultVC4a3.push_back(ii);
+                    }
+                }
+            }
+            break;
+        }
+    }
     
     
     mulock(LOCK,&mut_cnt);
@@ -252,8 +504,9 @@ void* io(void* args){
     
     // start your output here
     //g.printGraph();
-    printResult("a1",resultVC4a1);
-    printResult("a2",resultVC4a2);
+    printResult("CNF-SAT-VC",resultVC4a3);
+    printResult("APPROX-VC-1",resultVC4a2);
+    printResult("APPROX-VC-2",resultVC4a1);
     
     
     mulock(LOCK,&mut_cnt);
