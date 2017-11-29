f212dc7f6409e473aad3a606f4451fda9951f207
胡其成 <huqicheng@huqicheng.local>
2017-11-29 12:44:51 -0500

    freopen

--- rgen.cpp
+++ rgen.cpp
@@ -14,13 +14,58 @@ int main(int argc, char **argv){
     int cnt=0;
     while(cnt<10){
         
+    
+        int pid_rgen, pipe_rgen[2];
+        pipe(pipe_rgen);
+        pid_rgen = fork();
+        if(pid_rgen < 0){
+            std::cerr << "Error: Failed to fork rgen" << endl;
+            exit(1);
         
-        execvp("./graphGen",argv);
+        }else if(pid_rgen == 0){
+            //bind stdout to the write-end of pipe
+            dup2(pipe_rgen[1], STDOUT_FILENO);
         
+            //close pipes in this process
+            close(pipe_rgen[0]);
+            close(pipe_rgen[1]);
+        
+            execvp("./graphGen",argv);
+        
+            // exit with success
+            return 0;
+        }
+        
+        int rgen_read = fork();
+        if(rgen_read == 0)
+        {
+            //bind file stream read_from_a1 to read-end of pipe_a1
+            FILE* py2stream = fdopen(pipe_rgen[0],"r");
+            char* line = NULL;
+            size_t bytes = 0;
+            while(getline(&line, &bytes, py2stream) != -1)
+            {
+                cout << line;
+                
+                
+            }
+            
+            //close file stream
+            fclose(py2stream);
+            
+            
+            return 0;
+            
+            
+        }else if(pid_a1_read < 0){
+            std::cerr << "Error: Failed to fork reader of a1" << endl;
+            return 1;
+        }
 
         sleep(2);
         cnt ++;
-        
+        close(pipe_rgen[0]);
+        close(pipe_rgen[1]);
         
     }
     
